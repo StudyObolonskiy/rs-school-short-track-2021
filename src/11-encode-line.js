300d1ab9891fbb4bd333be6f2c61f629b61596d3
StudyObolonskiy <study.obolonskiy@gmail.com>
2021-04-11 23:39:28 +0200

    feat: complete 11 task

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,31 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  const arr = str.split('');
+  let count = 1;
+  let res = '';
+  arr.forEach((e, i) => {
+    if (e === arr[i - 1]) {
+      count += 1;
+    }
+    if (e !== arr[i - 1] && i !== 0) {
+      if (count > 1) {
+        res += count + arr[i - 1];
+      } else {
+        res += arr[i - 1];
+      }
+      count = 1;
+    }
+    if (i === arr.length - 1) {
+      if (count > 1) {
+        res += count + e;
+      } else {
+        res += e;
+      }
+    }
+  });
+  return res;
 }
 
 module.exports = encodeLine;
